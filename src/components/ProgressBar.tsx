85114f913d705280532ac8e671ae6b0599a0835d
aitegin <zhanybaev1211@gmail.com>
2023-04-21 08:31:12 +0600

    quiz page is half responsive

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -5,35 +5,25 @@ interface IProgressBar{
 const ProgressBar = ({completed}:IProgressBar) => {
   
     const containerStyles = {
-        height: 5,
-        width: '100%',
-        backgroundColor: "white",
-        borderRadius: 50,
-        margin: 50,
-      }
-    
-      const fillerStyles = {
-        height: '7px',
-        width: `${completed}%`,
-        backgroundColor: '#51cf66',
-        borderRadius: '10px',
-        transform:'translateY(-1px)'
+      height: 5,
+      width: '100%',
+      backgroundColor: "white",
+      borderRadius: 50,
+      margin: 50,
     }
-  
-    const labelStyles = {
-      color: 'white',
-      padding:5,
-      fontSize:'13px',
-      fontStyle:'italic',
-      display:'block',
-      transform: 'translateY(-30px)',
-      minWidth:'81px'
+    
+    const fillerStyles = {
+      height: '7px',
+      width: `${completed}%`,
+      backgroundColor: '#51cf66',
+      borderRadius: '10px',
+      transform:'translateY(-1px)'
     }
 
     return (
       <div style={{...containerStyles}}>
         <div style={{...fillerStyles, textAlign:'right'}}>
-            <span style={{...labelStyles, wordBreak:'normal'}}>
+            <span className="progressBar__label">
               {`${completed}% complete`}
             </span>
         </div>
--- src/components/QuizHeader.tsx
+++ src/components/QuizHeader.tsx
@@ -5,7 +5,9 @@ const QuizHeader = ({topicName}:{topicName:string}) => {
     return (
         <header>
             <nav>
-                <div className="nav__item nav__item-title">{topicName.replace("-"," ")} quiz</div>
+                <div className="nav__item nav__item-title">
+                    {`${topicName.replace("-"," ")} quiz`}
+                </div>
                 <button onClick={()=>updateProgress(topicName, initialProgress)} className="nav__item nav__item-btn">
                     Reset quiz
                 </button>
